a6c82b426ef7bc0deca80eb4bcf19bfad2d9be99
Emma <emma.shemtov@runi.ac.il>
2025-11-11 22:57:06 +0200

    Collatz

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,25 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+    public static void main(String[] args) {
+        String word = args[0].toUpperCase();
+        int n = Integer.parseInt(args[1]);
+
+        String anLetters = "AEFHILMNORSX";
+
+        // Step 1: Give me a/an lines
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if (anLetters.indexOf(ch) != -1) {
+                System.out.println("Give me an " + ch + ": " + ch + "!");
+            } else {
+                System.out.println("Give me a " + ch + ": " + ch + "!");
+            }
         }
+
+        // Step 2: The spell section
+        System.out.println("What does that spell?");
+        for (int i = 0; i < n; i++) {
+            System.out.println(word + "!!!");
+        }
+    }
 }
